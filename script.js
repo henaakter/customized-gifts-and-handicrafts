50858fe38c77f2d021e8b00f137933a4099fcc9f
henaakter <henaakterkoli65@gmail.com>
2024-11-30 03:59:54 +0600

    added review, contact and footer

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    searchForm.classList.toggle('active');
-    cartItem.classList.toggle('active');
+    // searchForm.classList.toggle('active');
+    // cartItem.classList.toggle('active');
 }
 
-let searchForm = document.querySelector('.serach-form');
+let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
-    navbar.classList.toggle('active');
-    cartItem.classList.toggle('active');
+    // navbar.classList.toggle('active');
+    // cartItem.classList.toggle('active');
 }
 
 let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
-    navbar.classList.toggle('active');
-    searchForm.classList.toggle('active');
+    // navbar.classList.toggle('active');
+    // searchForm.classList.toggle('active');
     
 }
 
