ca86b1312416885312071df0e63fb777b4583dde
henaakter <henaakterkoli65@gmail.com>
2024-12-31 00:39:59 +0600

    added firebase config

---
+++ firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7qCAe1nhQc9j9cmybmeNKWWFQoUSWwRs",
+  authDomain: "customized-gifts-n-handicrafts.firebaseapp.com",
+  projectId: "customized-gifts-n-handicrafts",
+  storageBucket: "customized-gifts-n-handicrafts.firebasestorage.app",
+  messagingSenderId: "687169127703",
+  appId: "1:687169127703:web:f9873b0e2fde5bc5362352"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
